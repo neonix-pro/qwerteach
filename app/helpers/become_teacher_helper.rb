a6582defd2e0277aa95ef93f74536acd16493e38
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-01-30 13:08:47 +0100

    progressbar look ok

--- app/helpers/become_teacher_helper.rb
+++ app/helpers/become_teacher_helper.rb
@@ -2,19 +2,19 @@ module BecomeTeacherHelper
 
 def tutorial_progress_bar
   content_tag(:section, class: "container") do
-    content_tag(:div, class: "row navigator") do
-      content_tag(:ol) do
-        wizard_steps.collect do |every_step|
+    content_tag(:div, class: "row navigator become-teacher-progress-bar ") do
+      content_tag(:ul) do
+        wizard_steps.each_with_index do |every_step, index|
           finished_state = "unfinished"
           finished_state = "current"  if every_step == step
           finished_state = "finished" if past_step?(every_step)
-          class_str = "pull-left "
+          class_str = "become-teacher-step "
           concat(
             content_tag(:li, class: class_str + finished_state) do
-              if finished_state == 'finished'
-                link_to '<i class="fa fa-check"></i>'.html_safe+I18n.t(every_step, scope: 'become_teacher'), wizard_path(every_step)
-              else
-                link_to I18n.t(every_step, scope: 'become_teacher'), wizard_path(every_step)
+              link_to wizard_path(every_step) do
+                name = content_tag(:div, I18n.t(every_step, scope: 'become_teacher'), class:'step-name')
+                number = content_tag(:span, "#{index+1}", class:'step-number')
+                number + name
               end
             end
           )
