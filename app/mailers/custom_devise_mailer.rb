dad391ec0756bca4803d2c206f2bf6b268aa972e
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-02-10 15:28:35 +0100

    add sendgrid templates

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -4,7 +4,26 @@ class CustomDeviseMailer < Devise::Mailer
   default template_path: 'devise/mailer'
 
   def confirmation_instructions(record, token, opts={})
+    template = '6fa5dbfc-ccf8-4236-ae4b-d9d96e84a2f6'
     # insert here customizations
+    opts= {"X-SMTPAPI" => {"filters" => {
+              "templates" => {
+                "settings" => {
+                  "enable" => 1, "template_id" =>template}
+                }
+              }
+            }.to_json
+    }
+    super
+  end
+  def reset_password_instructions(record, token, opts={})
+    opts =  {"filters" => {
+              "templates" => {
+                "settings" => {
+                  "enable" => 1, "template_id" => "208ffcb4-5129-4111-8aa2-1353abde1bc2"}
+                }
+              }
+            }.to_json
     super
   end
 end
