e4b4564643d569801a2dfff5ab48705fd6988e9f
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-03-10 11:10:41 +0100

    added resque worker to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,6 @@ rm tmp/pids/server.pid
 
 rackup private_pub.ru -s thin -E production -o 0.0.0.0 -D
 BACKGROUND=yes rake resque:scheduler
+PIDFILE=./resque.pid BACKGROUND=yes QUEUE=* rake resque:work
 
 rails server -e development -p 3000 -b '0.0.0.0'
