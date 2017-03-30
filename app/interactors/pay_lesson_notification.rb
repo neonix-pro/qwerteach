885f67762da461eea02dd67ff97c9bb2d29e29d3
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-03-30 16:44:49 +0200

    switched notify of payment after and before the lesson happened added payment by wallet transfer and bank wire in payment method in notificationof payment

--- app/interactors/pay_lesson_notification.rb
+++ app/interactors/pay_lesson_notification.rb
@@ -12,9 +12,9 @@ module PayLessonNotification
     return if errors.any?
     LessonNotificationsJob.perform_async(:notify_teacher_about_booking, lesson) if @created
     if lesson.past?
-      LessonNotificationsJob.perform_async(:notify_teacher_about_student_pay_lesson_before, lesson)
-    else
       LessonNotificationsJob.perform_async(:notify_teacher_about_student_pay_lesson_after, lesson)
+    else
+      LessonNotificationsJob.perform_async(:notify_teacher_about_student_pay_lesson_before, lesson)
     end
     NotificationsMailer.send_payment_details_to_student(payment).deliver_later
   end
