54cc94d6444bd793dfe155e3d90445a66a36cabf
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-04-11 11:34:34 +0200

    setup onboarding

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -7,7 +7,10 @@ class OnboardingController < ApplicationController
     @user = current_user
     case step
       when :welcome
-
+      when :picture
+      when :topics
+        @topic_groups = TopicGroup.first(6)
+        @teachers = Teacher.order(score: :desc).first(8)
     end
     render_wizard
   end
@@ -16,7 +19,15 @@ class OnboardingController < ApplicationController
     @user = current_user
     case step
       when :welcome
+      when :picture
+        @user.update_attributes(user_params)
     end
     render_wizard @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:crop_x, :crop_y, :crop_w, :crop_h, :firstname, :lastname,
+                                 :description, :avatar, :phone_number, :phone_country_code)
   end
 end
