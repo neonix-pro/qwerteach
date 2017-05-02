12ac1d24ffd2e91346bac4fef08e15bab1ce70e3
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-05-02 10:52:16 +0200

    fixed migration crating interests table

--- db/migrate/20170501121938_create_interests.rb
+++ db/migrate/20170501121938_create_interests.rb
@@ -1,8 +1,8 @@
 class CreateInterests < ActiveRecord::Migration
   def change
     create_table :interests do |t|
-      t.belongs_to :student, index: true, foreign_key: true
-      t.belongs_to :topic, index: true, foreign_key: true
+      t.references :student, null: false
+      t.references :topic, null: false
 
       t.timestamps null: false
     end
