bcacfa27329bd7529e234c3f8d4492a5a33cdd3a
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-05-31 10:54:21 +0200

    faye over ssl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm tmp/pids/server.pid
 
-rackup private_pub.ru -s thin -E production -o 0.0.0.0 -D
+#rackup private_pub.ru -s thin -E production -o 0.0.0.0 -D
+thin -C config/private_pub_thin.yml  -o 0.0.0.0 -D start
 BACKGROUND=yes rake resque:scheduler
 PIDFILE=./resque.pid BACKGROUND=yes QUEUE=* rake resque:work
 
