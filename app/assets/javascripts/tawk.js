6fdb138c0e1468c23386451f9e6929ed1ce258d7
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-06-01 15:45:27 +0200

    ssetup user name in tawk if logged in

--- app/assets/javascripts/tawk.js
+++ app/assets/javascripts/tawk.js
@@ -1,7 +1,14 @@
-
 $(document).on('turbolinks:load', function(){
     $('.open-tawk').click(function(e){
         e.preventDefault();
         Tawk_API.maximize();
     });
+
+    if ($('meta[name=user-email]').attr("content").length){
+        Tawk_API.visitor = {
+            email: $('meta[name=user-email]').attr("content"),
+            name: $('meta[name=user-name]').attr("content")
+        };
+    }
+
 });
