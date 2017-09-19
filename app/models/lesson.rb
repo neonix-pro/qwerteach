c144788915216e9af656da545064348776be83a1
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-09-19 16:11:59 +0200

    show # of lessons given and taken + total of money paid / earned

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -33,6 +33,8 @@ class Lesson < ActiveRecord::Base
   scope :to_answer, -> { pending.locked.future } # lessons where we're waiting for an answer
   scope :to_unlock, -> { created.locked.past } # lessons where we're waiting for student to unlock money
   #scope :to_pay, ->{created.payment_pending.past} # lessons that haven't been prepaid and student needs to pay
+  scope :free, -> {where(price: 0)}
+  scope :not_free, -> {where('price >0')}
 
   scope :to_review, ->(user){created.locked_or_paid.past.joins('LEFT OUTER JOIN reviews ON reviews.subject_id = lessons.teacher_id
     AND reviews.sender_id = lessons.student_id')
