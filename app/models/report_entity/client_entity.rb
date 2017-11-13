bcb5c2165bdfb5edd71e7aef5bb3cd7b93475aae
Rudskikh Ivan <shredder.rull@gmail.com>
2017-11-13 10:12:20 +0700

    Avatar image. Pretty dates in clients reports

--- app/models/report_entity/client_entity.rb
+++ app/models/report_entity/client_entity.rb
@@ -5,5 +5,11 @@ class ReportEntity
     attribute :last_lesson_date, Type::DateTime.new
     attribute :first_lesson_date, Type::DateTime.new
 
+    attribute :avatar_file_name, Type::String.new
+
+
+    has_attached_file :avatar,
+      styles: {:small => "100x100#", medium: "300x300>", :large => "500x500>"},
+      url: "/system/avatars/:hash.:extension", hash_secret: "laVieEstBelllllee", :hash_data => "/:attachment/:id/:style"
   end
 end
