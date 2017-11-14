ae3700678690cfad35489ccd3a2cae19f1a09c68
Rudskikh Ivan <shredder-rull@yandex.ru>
2017-11-14 17:22:05 +0700

    Top teachers

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -6,8 +6,9 @@ module Admin
     def index
       @report = DashboardReport.run(report_params)
       @entities = @report.result
-      @postulling_teachers = Teacher.joins(:postulation).where(postulations: { admin_id: nil })
+      @postulling_teachers = postulling_teachers
       @online_users = User.online
+      @top_teachers = top_teachers
 
       @stats ||= Stats.new(days)
     end
@@ -18,20 +19,30 @@ module Admin
       (params[:days].try(:to_i) || 30)
     end
 
-    def start_date
-      days.days.ago
-    end
-
-    def end_date
-      Time.current
-    end
-
     def report_params
       { days: days }
     end
 
     def nav_link_state(resource)
       resource == :home ? :active : :inactive
+    end
+
+    def top_teachers
+      Teacher
+        .select('users.*, count(lessons.id) as lessons_count, sum(lessons.price) as lessons_amount')
+        .joins(:lessons_given)
+        .where(lessons: {
+          status: Lesson.statuses[:created],
+          time_start: (days - 1).days.ago..Time.current
+        })
+        .group('users.id').order('count(lessons.id) desc').limit(5)
+    end
+
+    def postulling_teachers
+      Teacher
+        .includes(:postulation)
+        .references(:postulation)
+        .where(postulations: { admin_id: nil })
     end
 
   end
