727bd1a6828a87ce2d9266dca3f726c6bdc1cec6
Rudskikh Ivan <shredder-rull@yandex.ru>
2017-11-14 23:36:02 +0700

    Fix some tests

--- spec/controllers/galleries_controller_spec.rb
+++ spec/controllers/galleries_controller_spec.rb
@@ -1,68 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe GalleriesController, type: :controller do
-  login_user
-  describe GalleriesController do
 
-    before :each do
-      request.env['devise.mapping'] = Devise.mappings[:user]
-    end
-    it "should have a current_user" do
-      expect(subject.current_user).to_not eq(nil)
-    end
-    it "should get show : his gallery" do
-      get 'show', :id => subject.current_user.gallery.id
-      expect(response).to be_success
-    end
-    it "should get show : other gallery" do
-      get 'show', :id => Gallery.second.id
-      expect(response).to be_success
-    end
-    it "should get edit : his gallery" do
-      get 'edit', :id => subject.current_user.gallery.id
-      expect(response).to be_success
-    end
-    it "shouldn't get show : inexistant gallery" do
-      expect {
-        get 'show', :id => 34
-      }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-    it "should put edit : his gallery" do
-      put 'edit', :id => subject.current_user.gallery.id, :images => []
-      expect(response).to be_success
-    end
-    it "shouldn't get edit : other gallery" do
-      get 'edit', :id => Gallery.first.id
-      expect(response).to redirect_to root_path
-    end
-    it "shouldn't get edit : other gallery" do
-      put 'edit', :id => Gallery.first.id, :images => []
-      expect(response).to redirect_to root_path
-    end
+  let(:user) { create(:student) }
+  let(:gallery){ user.gallery }
+  let(:other_gallery){ create(:student).gallery }
+  before :each do
+    sign_in user
+  end
+
+  it "should have a current_user" do
+    expect(subject.current_user).to_not eq(nil)
+  end
+  it "should get show : his gallery" do
+    get 'show', :id => gallery.id
+    expect(response).to be_success
+  end
+  it "should get show : other gallery" do
+    get 'show', :id => other_gallery.id
+    expect(response).to be_success
+  end
+  it "should get edit : his gallery" do
+    get 'edit', :id => gallery.id
+    expect(response).to be_success
+  end
+  it "shouldn't get show : inexistant gallery" do
+    expect {
+      get 'show', :id => 34
+    }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+  it "should put edit : his gallery" do
+    put 'edit', :id => gallery.id, :images => []
+    expect(response).to be_success
+  end
+  it "shouldn't get edit : other gallery" do
+    get 'edit', :id => other_gallery.id
+    expect(response).to redirect_to root_path
+  end
+  it "shouldn't get edit : other gallery" do
+    put 'edit', :id => other_gallery.id, :images => []
+    expect(response).to redirect_to root_path
   end
 end
 
 RSpec.describe GalleriesController, type: :controller do
-  describe GalleriesController do
-    before :each do
-      request.env['devise.mapping'] = Devise.mappings[:user]
-    end
-    it "shouldn't have a current_user" do
-      expect(subject.current_user).to eq(nil)
-    end
-    it "shouldn't get show" do
-      get 'show', :id => Gallery.first.id
-      expect(response).to redirect_to new_user_session_path
-    end
-    it "shouldn't get edit" do
-      get 'edit', :id => Gallery.second.id
-      expect(response).to redirect_to root_path
-    end
-    it "shouldn't get show : inexistant gallery" do
-      expect {
-        get 'show', :id => 34
-      }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-  end
+  let(:user) { create(:student) }
+  let(:gallery){ user.gallery }
+  let(:other_gallery){ create(:student).gallery }
 
+  it "shouldn't have a current_user" do
+    expect(subject.current_user).to eq(nil)
+  end
+  it "shouldn't get show" do
+    get 'show', :id => gallery.id
+    expect(response).to redirect_to new_user_session_path
+  end
+  it "shouldn't get edit" do
+    get 'edit', :id => gallery.id
+    expect(response).to redirect_to root_path
+  end
+  it "shouldn't get show : inexistant gallery" do
+    expect {
+      get 'show', :id => 34
+    }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
