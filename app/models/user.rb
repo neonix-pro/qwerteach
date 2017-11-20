dbfe9a2d9a84918f965889176ab789c6063d1c6c
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-11-20 10:57:32 +0100

    fix # of characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   acts_as_token_authenticatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :lockable, :validatable,
-         :lastseenable, :confirmable
+         :lastseenable, :confirmable, password_length: 8..128
           #, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
   phony_normalize :phone_number, as: :full_number, default_country_code: :phone_country_code
