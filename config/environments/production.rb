837c72e6a903ed6b2a7fd01fbd7ea8dad1f4d600
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-11-27 11:57:05 +0100

    fix bug lien notificationswq

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@example.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'qwerteach.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'qwerteach.com' }
 
   config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
