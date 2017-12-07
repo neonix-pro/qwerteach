65c26e2ab3531a7dbadda0dff604ee3d2dc0185a
Aurélie Merckx <roulili.la.terreur@gmail.com>
2017-12-07 17:25:05 +0100

    bugfix payment prof

--- app/interactors/pay_teacher.rb
+++ app/interactors/pay_teacher.rb
@@ -21,7 +21,7 @@ class PayTeacher < ActiveInteraction::Base
           raise ActiveRecord::Rollback
         end
         payment.status = 'paid'
-        payment.transfer_prof_id = transfer.result.id
+        #payment.transfer_prof_id = transfer.result.id
         if !payment.save
           self.errors.merge! payment.errors
           Rails.logger.debug("Impossible de sauver le payement. #{payment.errors.full_messages.to_sentence}")
