e3e35ae7b86d9f620ab706205ad5cbe2ea8c3a28
Aurélie Merckx <roulili.la.terreur@gmail.com>
2018-03-30 14:16:45 +0200

    fix bug payments lessons at 0

--- app/jobs/pay_teachers.rb
+++ app/jobs/pay_teachers.rb
@@ -5,10 +5,15 @@ class PayTeachers
     #fetch lessons
     @lessons = Lesson.created.locked.where("time_start < :time ", {:time => 3.days.ago})
     @lessons.each do |l|
-      pay_teacher = PayTeacher.run(user: l.student, lesson: l)
-      unless pay_teacher.valid?
-        Rails.logger.debug("Impossible de payer le prof pour le cours #{l.id}. #{pay_teacher.errors.full_messages.to_sentence}")
+      if l.price == 0
+        l.payments.update_all(status: 2)
+      else
+        pay_teacher = PayTeacher.run(user: l.student, lesson: l)
+        unless pay_teacher.valid?
+          Rails.logger.debug("Impossible de payer le prof pour le cours #{l.id}. #{pay_teacher.errors.full_messages.to_sentence}")
+        end
       end
+
     end
   end
 end
