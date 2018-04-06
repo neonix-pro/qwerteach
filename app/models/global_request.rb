b99a3f252d0160f287c55a4e9cefdfea879b816b
PoulyCroc <esga.retard@gmail.com>
2018-04-06 09:05:16 +0200

    the last commit ?

--- app/models/global_request.rb
+++ app/models/global_request.rb
@@ -2,6 +2,7 @@ class GlobalRequest < ActiveRecord::Base
   belongs_to :student, foreign_key: :user_id
   belongs_to :topic
   belongs_to :level
+  belongs_to :prefix_number
 
   validates :description, length: { minimum: 50 }
   #0 => open ; 1 => closed
