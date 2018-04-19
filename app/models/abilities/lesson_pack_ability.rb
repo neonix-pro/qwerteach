c26d3b1b71db35cfb72ca508dbe646706f068d74
Aurélie Merckx <roulili.la.terreur@gmail.com>
2018-04-19 10:10:48 +0200

    changed inex of lesson packs

--- app/models/abilities/lesson_pack_ability.rb
+++ app/models/abilities/lesson_pack_ability.rb
@@ -7,7 +7,7 @@ class LessonPackAbility
       can :confirm, LessonPack
     end
     can %i[show edit delete update propose], LessonPack, teacher_id: user.id, status: %w[draft declined]
-    can :show, LessonPack, student_id: user.id, status: 'pending_student'
+    can :show, LessonPack, student_id: user.id #, status: 'pending_student'
     can [:pay, :payment, :finish_payment], LessonPack, student_id: user.id, status: 'pending_student'
     can :reject, LessonPack, student_id: user.id, status: 'pending_student'
   end
