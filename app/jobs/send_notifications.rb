e3609bd763f42d47f08455037062539f0a54917d
Aurélie Merckx <roulili.la.terreur@gmail.com>
2018-05-21 14:42:53 +0200

    fixed broken link on lesson_mailer send email only for notifications related to messages  Please enter the commit message for your changes. Lines starting

--- app/jobs/send_notifications.rb
+++ app/jobs/send_notifications.rb
@@ -12,7 +12,7 @@ class SendNotifications
     # end
 
     ActiveRecord::Base.transaction do
-      receipts = user.receipts.where(is_read: false).lock(true)
+      receipts = user.receipts.joins(:notification).where(is_read: false, mailboxer_notifications:{type: 'messages'}).lock(true)
       # receipts = user.receipts.joins(:notification).joins(:sender)
         # .where(is_read: false).where(mailboxer_notifications:{type: 'messages'})
 
