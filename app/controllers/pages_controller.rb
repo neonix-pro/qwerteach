f33641930ea520a72334a2de1d44e28b1d149446
Aurélie Merckx <roulili.la.terreur@gmail.com>
2018-06-22 09:32:41 +0200

    fix nouvelle landing page add privacy conditions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,7 +23,9 @@ class PagesController < ApplicationController
 	end
 
   def abtest
-		index
+		@featured_teachers_landing =  User.where(postulance_accepted: true).where.not(avatar_score: nil).limit(13).order(avatar_score: :desc)
+		@featured_reviews =  Review.where.not(:review_text => "").order("created_at DESC").uniq.limit(3)
+		@featured_topics = TopicGroup.where(featured: true) + Topic.where(featured: true)
 		@featured_teachers_landing =  User.where(postulance_accepted: true).where.not(avatar_score: nil).limit(7).order(avatar_score: :desc)
 		render template: "pages/#{params[:page]}/#{params[:version]}", layout: 'no-navbar'
 		#render template: "pages/#{params[:page]}/winner", layout: 'no-navbar'
